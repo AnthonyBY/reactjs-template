b2e2cc49d602fc07c9f379b35a1034a5f020c6bd
Anton Marchanka <anthonymarchenko@gmail.com>
2024-10-12 10:38:12 +0400

    feat: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import mkcert from "vite-plugin-mkcert"
-// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,3 +24,4 @@ export default defineConfig({
     host: true,
   },
 });
+
